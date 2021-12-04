49e50013ca63053fc9a385577db0255fcde99ca7
Mradeker <marcel.raedeker@gmail.com>
2021-12-04 01:34:04 +0100

    Added globals to hold config data

--- globals.c
+++ globals.c
@@ -1,7 +1,14 @@
 
+#define GL_SequnceCap 1000
+
 typedef enum {
     KeyUp,
     KeyDown
 } KeyPressMode;
 
 LARGE_INTEGER GL_PerformanceFrequency;
+char GL_Sequence[GL_SequnceCap];
+unsigned GL_SequnceLength;
+double GL_Cycle;
+double GL_CycleVariance;
+double GL_HoldTime;
